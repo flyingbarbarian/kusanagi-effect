2f9c9ac089ad02046e18458b2f8354d3d04ba074
flyingbarbarian <50474981+flyingbarbarian@users.noreply.github.com>
2023-01-10 23:48:43 +0900

    動画を表示する

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # kusanagi-effect
 動画から人の手の部分を検出し、炎の動画を重ねる
+
+## ビルド方法
+### build用のディレクトリの作成
+```
+mkdir build && cd build
+```
+### buildの設定
+```
+cmake ../
+```
+
+### build
+```
+cmake --build .
+```
+
+### アプリケーションの実行
+```
+./main
+```
--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
-# include<stdio.h>
+#include <iostream>
+//
+#include <string>
 
-int main() {
-    printf("aaaaa\n");
+#include <opencv2/core.hpp>
+// window制御とか
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+// argcにはコマンドライン引数の個数が渡される。
+// argv[]にはコマンドライン引数が渡される
+int main(int argc, char *argv[])
+{
+    // 動画を表示するウィンドウ
+    cv::namedWindow("kusanagi-window", cv::WINDOW_AUTOSIZE);
+
+    // 動画を読み込む
+    const std::string filePath = "../hand.mp4";
+    cv::VideoCapture cap(filePath, cv::CAP_FFMPEG);
+
+    std::cout << filePath << std::endl;
+
+    if (!cap.isOpened())
+    {
+        std::cout << "動画を開けません" << std::endl;
+        return -1;
+    }
+
+    // 動画を再生する
+    cv::Mat frame;
+    for(;;) {
+        cap >> frame;
+        if (frame.empty()) {
+            break;
+        }
+
+        cv::imshow("kusanagi-window", frame);
+        if ((char)cv::waitKey(33) >= 0) {
+            break;
+        }
+    }
+    return 0;
 }
